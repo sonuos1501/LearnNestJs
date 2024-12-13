cc28b88ef52b842b54954262e3850b5d35f87744
sonuos1501 <son.uospt@gmail.com>
2024-12-13 10:20:13 +0700

    co ban

---
+++ LearnTypeScript/coban/app.js
@@ -0,0 +1,15 @@
+/// Kiểu String
+var message = "Hello, TypeScript!";
+console.log("Message: " + message);
+/// Kiểu number: Số nguyên và số thực
+var num = 2;
+console.log("Num: " + num);
+/// Kiểu boolean: giá trị đúng sai
+var bool = true;
+console.log("Boolean: " + bool);
+/// Kiểu null: kiểu không có giá trị
+var n = null;
+console.log("Null: " + n);
+/// Kiểu undefined: kiểu không xác định
+var u = undefined;
+console.log("Undefined: " + u);
---
+++ LearnTypeScript/coban/app.ts
@@ -0,0 +1,19 @@
+/// Kiểu String
+const message: string = "Hello, TypeScript!"
+console.log("Message: " + message)
+
+/// Kiểu number: Số nguyên và số thực
+const num: number = 2
+console.log("Num: "  + num)
+
+/// Kiểu boolean: giá trị đúng sai
+const bool: boolean = true
+console.log("Boolean: " + bool)
+
+/// Kiểu null: kiểu không có giá trị
+const n = null
+console.log("Null: " + n)
+
+/// Kiểu undefined: kiểu không xác định
+const u = undefined
+console.log("Undefined: " + u)
