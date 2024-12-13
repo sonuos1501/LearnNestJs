ac930d9c45fcce7d001db7f9b00afa184b5b4508
sonuos1501 <son.uospt@gmail.com>
2024-12-13 11:01:32 +0700

    build

---
+++ LearnTypeScript/ham/app.js
@@ -0,0 +1,5 @@
+/// Hàm cơ bản
+function add(a, b) {
+    return a + b;
+}
+console.log("Add: " + add(2, 8));
---
+++ LearnTypeScript/ham/app.ts
@@ -0,0 +1,6 @@
+/// Hàm cơ bản
+function add(a: number, b: number): number {
+    return a + b
+}
+
+console.log("Add: " + add(2, 8))
